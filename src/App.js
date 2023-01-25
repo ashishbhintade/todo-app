697138b796b8a9e1d5c27efafff13bdcb0fe3e0d
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2023-01-25 17:58:05 +0530

    Styling elements

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Todo from "./Todo";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500p] w-full m-auto rounded-md shdow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-3 font-bold`,
 };
 
 function App() {
