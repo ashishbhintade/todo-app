1bb7502b03c42207b04eb4f620ede74a387226d3
Ashish Bhintade <68814285+ashishbhintade@users.noreply.github.com>
2023-01-26 18:35:55 +0530

    Updating todo in firebase

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import Todo from "./Todo";
 import { db } from "./firebase";
-import { query, collection, onSnapshot } from "firebase/firestore";
+import {
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
@@ -31,6 +37,11 @@ function App() {
     return () => unsubscribe();
   }, []);
   //Update todo in firebase
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), {
+      completed: !todo.completed,
+    });
+  };
   //Delete todo
   return (
     <div className={style.bg}>
@@ -44,7 +55,7 @@ function App() {
         </form>
         <ul>
           {todos.map((todo, index) => (
-            <Todo key={index} todo={todo} />
+            <Todo key={index} todo={todo} toggleComplete={toggleComplete} />
           ))}
         </ul>
         <p className={style.count}>You have 2 todos</p>
